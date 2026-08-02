708ab7d89f21589210211de62cde19a70cdbba86
RobyCajulis <robycajulis01@gmail.com>
2026-08-02 22:57:02 +0800

    Fixed no Contact

--- src/components/bg.tsx
+++ src/components/bg.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import Chill from "../assets/Chill.mp3";
 
 export default function BackgroundMusic() {
